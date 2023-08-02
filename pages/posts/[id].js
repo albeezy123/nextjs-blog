b9a01d686b10ee69c9ad16f1c1209a20f364155c
albeezy123 <116127933+albeezy123@users.noreply.github.com>
2023-08-02 04:40:11 -0400

    Initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,47 @@
+import Date from '../../components/date';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Keep the existing code here */}
+
+      {/* Replace {postData.date} with this */}
+      <Date dateString={postData.date} />
+
+      {/* Keep the existing code here */}
+    </Layout>
+  );
+}
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      {/* Keep the existing code here */}
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
