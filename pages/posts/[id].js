315376e1761f43255a69a402b55b4dd9458a0c39
albeezy123 <116127933+albeezy123@users.noreply.github.com>
2023-08-02 19:50:03 -0400

    initival commit

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,14 @@
 import Date from '../../components/date';
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {/* Keep the existing code here */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
 
       {/* Replace {postData.date} with this */}
       <Date dateString={postData.date} />
@@ -12,36 +17,20 @@ export default function Post({ postData }) {
     </Layout>
   );
 }
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head';
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {/* Add this <Head> tag */}
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-
-      {/* Keep the existing code here */}
-    </Layout>
-  );
-}
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
     fallback: false
-  }
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = getPostData(params.id);
   return {
     props: {
       postData
     }
-  }
+  };
 }
